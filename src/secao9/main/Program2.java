2deeaa2dfeb3835e22525d41f78ca47ac3def35e
William Santana <williamandrade1058@gmail.com>
2024-08-15 16:48:06 -0300

    sobrecarga

--- src/secao9/main/Program2.java
+++ src/secao9/main/Program2.java
@@ -1,10 +1,11 @@
 package secao9.main;
 
-import java.util.Locale;
-import java.util.Scanner;
 import secao9.entities.Product;
 
-public class Program {
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program2 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
@@ -17,17 +18,15 @@ public class Program {
         System.out.print("Price: ");
         double price = sc.nextDouble();
 
-        System.out.print("Quantity in stock: ");
-        int quantity = sc.nextInt();
 
-        Product product = new Product(name, price, quantity);
+        Product product = new Product(name, price);
         System.out.println();
 
         System.out.println("Product data: " + product);
         System.out.println();
 
         System.out.print("Enter the number of products to be added in stock: ");
-        quantity = sc.nextInt();
+        int quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
