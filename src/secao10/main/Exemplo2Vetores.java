f42ecd89293058f46a7ca6f7758bfbd52d819145
William Santana <williamandrade1058@gmail.com>
2024-08-22 17:25:46 -0300

    vetores parte 2

---
+++ src/secao10/main/Exemplo2Vetores.java
@@ -0,0 +1,33 @@
+package secao10.main;
+
+import secao10.entities.Product;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exemplo2Vetores {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        var number = sc.nextInt();
+        Product[] heightVector = new Product[number];
+
+        for (int i = 0; i < heightVector.length; i++) {
+            System.out.println("Enter a name product and price: ");
+            var name = sc.next();
+            var price = sc.nextDouble();
+            heightVector[i] = new Product(name, price);
+        }
+
+        double sum = 0;
+        for (Product product : heightVector) {
+            sum += product.getPrice();
+        }
+        double average = sum / number;
+        System.out.println(Arrays.toString(heightVector));
+        System.out.println("Average = " + average);
+        sc.close();
+    }
+}
