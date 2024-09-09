02c315e8df4109d85257fba4f69616813ea58dc1
William Santana <williamandrade1058@gmail.com>
2024-09-08 21:15:41 -0300

    trabalhando com datas

---
+++ src/secao11/main/InstanciacaoDataHora.java
@@ -0,0 +1,41 @@
+package secao11.main;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class InstanciacaoDataHora {
+    public static void main(String[] args) {
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
+
+        LocalDate d04 = LocalDate.parse("2024-09-08");
+        LocalDateTime d05 = LocalDateTime.parse("2024-09-08T01:30:26");
+        Instant d06 = Instant.parse("2024-09-08T04:30:26Z");
+        Instant d07 = Instant.parse("2024-09-08T04:30:26-03:00");
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDate d08 = LocalDate.parse("20/07/2022", fmt1);
+        LocalDateTime d09 = LocalDateTime.parse("20/07/2022 01:30", fmt2);
+
+        LocalDate d10 = LocalDate.of(2022, 7, 20);
+        LocalDateTime d11 = LocalDateTime.of(2022, 7, 20, 1, 30);
+
+
+        System.out.println("d01 = " + d01);
+        System.out.println("d02 = " + d02);
+        System.out.println("d03 = " + d03);
+        System.out.println("d04 = " + d04);
+        System.out.println("d05 = " + d05);
+        System.out.println("d06 = " + d06);
+        System.out.println("d07 = " + d07);
+        System.out.println("d08 = " + d08);
+        System.out.println("d09 = " + d09);
+        System.out.println("d10 = " + d10);
+        System.out.println("d11 = " + d11);
+    }
+}
