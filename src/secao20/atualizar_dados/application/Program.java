076be294e0a8547571bd4bdbbaa8321faf8234e5
William Santana <williamandrade1058@gmail.com>
2025-03-04 14:49:16 -0300

    atualizando dados

---
+++ src/secao20/atualizar_dados/application/Program.java
@@ -0,0 +1,38 @@
+package secao20.atualizar_dados.application;
+
+import secao20.atualizar_dados.db.DB;
+import secao20.recupera_dados.db.DbException;
+
+import java.beans.BeanDescriptor;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Program {
+    public static void main(String[] args) {
+        Connection conn;
+        PreparedStatement st = null;
+
+        try {
+          conn = DB.getConnection();
+
+          st = conn.prepareStatement("""
+                  UPDATE seller
+                  SET BaseSalary = BaseSalary + ?
+                  WHERE (DepartmentId = ?)
+                  """);
+          st.setDouble(1, 200.0);
+          st.setInt(2, 2);
+
+          int rowsAffected = st.executeUpdate();
+          System.out.println("Done! Rows affected: " + rowsAffected);
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
---
+++ src/secao20/atualizar_dados/db/DbException.java
@@ -0,0 +1,7 @@
+package secao20.atualizar_dados.db;
+
+public class DbException extends RuntimeException {
+    public DbException(String message) {
+        super(message);
+    }
+}
