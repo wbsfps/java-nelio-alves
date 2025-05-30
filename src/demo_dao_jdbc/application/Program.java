460e324cd4677d5af7ac7f1757736833888d41f1
William Santana <williamandrade1058@gmail.com>
2025-05-30 13:45:31 -0300

    seller entity class

--- src/demo_dao_jdbc/application/Program.java
+++ src/demo_dao_jdbc/application/Program.java
@@ -1,16 +1,16 @@
 package demo_dao_jdbc.application;
 
-import demo_dao_jdbc.db.DB;
 import demo_dao_jdbc.model.entities.Department;
-import secao20.transacoes.db.DbException;
+import demo_dao_jdbc.model.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Date;
+
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Department department = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+        System.out.println(department);
+        System.out.println(seller);
     }
 }
