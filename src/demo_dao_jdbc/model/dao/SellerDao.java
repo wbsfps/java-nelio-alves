c43760f8938d643f509b7949f24dcabd414a5204
William Santana <williamandrade1058@gmail.com>
2025-05-30 13:54:48 -0300

    DepartmentDao and SellerDao

---
+++ src/demo_dao_jdbc/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package demo_dao_jdbc.model.dao;
+
+import demo_dao_jdbc.model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert(Seller seller);
+    void update(Seller seller);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
